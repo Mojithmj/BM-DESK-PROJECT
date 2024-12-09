b0f68c7d401f2423fc046a0988b4ab7ef63972fa
m <you@example.com>
2024-12-09 12:48:55 +0530

    navbar changed

--- bm-desk/src/components/Navbar.jsx
+++ bm-desk/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import React, { useState,useRef,useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import image1 from "../assets/Group 1.jpg";
 import { FiSearch } from "react-icons/fi";
 import { PiBellThin } from "react-icons/pi";
 import Avatarimage from "../assets/Avatar.png";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
+import Ticketimageone from "../assets/Avatarimageone.png";
+import Ticketimagetwo from "../assets/Avatarimagetwo.png";
 import { useNavigate } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function Navbar() {
   const [circleDropdown, setCircleDropdown] = useState(false);
   const dropdownRef = useRef(null);
- 
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const openDialog = () => setIsDialogOpen(true);
 
   const toggleDropdown = () => {
     setCircleDropdown((prev) => !prev);
@@ -28,6 +40,66 @@ function Navbar() {
       document.removeEventListener("mousedown", handleOutsideClick);
     };
   }, []);
+
+  // notification drop down
+  const [notifycircleDropdown, setNotifyCircleDropdown] = useState(false);
+  const notifydropdownRef = useRef(null);
+
+  const [notifyisDialogOpen, setNotifyisDialogOpen] = useState(false);
+  const notifyopenDialog = () => setNotifyisDialogOpen(true);
+
+  const notifytoggleDropdown = () => {
+    setNotifyCircleDropdown((prev) => !prev);
+  };
+
+  const notifyhandleOutsideClick = (event) => {
+    if (
+      notifydropdownRef.current &&
+      !notifydropdownRef.current.contains(event.target)
+    ) {
+      setNotifyCircleDropdown(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", notifyhandleOutsideClick);
+  //   return () => {
+  //     document.removeEventListener("mousedown", notifyhandleOutsideClick);
+  //   };
+  // }, []);
+
+  const tickets = [
+    {
+      image: Ticketimageone,
+      title: "Ticket 3452",
+      description: "Ticket - TICKT-7424 has been resolved",
+      time: "36 min",
+    },
+    {
+      image: Ticketimagetwo,
+      title: "Ticket 3452",
+      description: "Ticket - TICKT-7424 has been resolved",
+      time: "1 Hr",
+    },
+    {
+      image: Ticketimageone,
+      title: "Ticket 3452",
+      description: "Ticket - TICKT-7424 has been resolved",
+      time: "Sept 06",
+    },
+    {
+      image: Ticketimagetwo,
+      title: "Ticket 3452",
+      description: "Ticket - TICKT-7424 has been resolved",
+      time: "1 Hr",
+    },
+    {
+      image: Ticketimageone,
+      title: "Ticket 3452",
+      description: "Ticket - TICKT-7424 has been resolved",
+      time: "Sept 06",
+    },
+  ];
 
   const navigate = useNavigate();
   return (
@@ -58,7 +130,68 @@ function Navbar() {
 
         {/* Right Section: Icons and Profile */}
         <div className="flex items-center gap-4">
-          <PiBellThin className="text-[19px] md:text-[23px] lg:text-[27px] 2xl:text-[31px]" />
+          {/* notifications */}
+          <div ref={notifydropdownRef}>
+            <PiBellThin
+              onClick={notifytoggleDropdown}
+              className="text-[19px] md:text-[23px] lg:text-[27px] 2xl:text-[31px]"
+            />
+          </div>
+          {notifycircleDropdown && (
+            <div className="absolute px-[16px] 2xl:py-[32px] lg:py-[28px] top-10 right-64 bg-white border border-gray-300 shadow-md rounded mt-1">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-5">
+                  <div className="flex text-[#165DFF] font-inter font-normal 2xl:text-[16px] lg:text-[16px] md:text-[12px] sm:text-[10px]  px-[12px] py-[8px] border-b-2 border-[#165DFF]  justify-center">
+                    Notifications
+                  </div>
+                  <div className="flex gap-2">
+                    <div className="border-[2px] w-full">
+                      <input
+                        type="text"
+                        placeholder="Search Notifications"
+                        className="bg-transparent placeholder-[#878A99] border-none outline-none text-sm font-normal px-[16px] py-[8px] rounded-[4px] border-[1px] border-[#BEDAFF]"
+                      />
+                    </div>
+                    <div className="bg-[#BEDAFF] rounded-[3px] px-[13px] py-[10px] justify-center text-[#165DFF]">
+                      <FiSearch className="w-[10px] h-[10px] md:w-[13px] md:h-[13px] lg:w-[18px] lg:h-[18px] 2xl:w-[20px] 2xl:h-[20px]" />
+                    </div>
+                  </div>
+                </div>
+
+                {/* ticket component */}
+                <div className="h-[270px] max-h-[280px] overflow-y-auto pr-2">
+                  <div className="w-full max-w-[400px] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[900px]">
+                    <div className="flex flex-col gap-[16px]">
+                      {tickets.map((ticket, index) => (
+                        <div
+                          key={index}
+                          className="flex items-center gap-[16px]"
+                        >
+                          <img
+                            src={ticket.image}
+                            className="rounded-[64px] w-[40px] h-[40px]"
+                            alt={`Ticket ${index + 1}`}
+                          />
+                          <div className="flex flex-col flex-grow">
+                            <p className="font-inter text-[14px] normal font-semibold text-[#1D2129]">
+                              {ticket.title}
+                            </p>
+                            <p className="font-inter text-[12px] normal font-normal text-[#86909C]">
+                              {ticket.description}
+                            </p>
+                          </div>
+                          <p className="font-inter text-[11px] whitespace-nowrap normal font-normal text-[#878A99]">
+                            {ticket.time}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
           <div>
             <img className="w-[40px] h-[40px]" src={Avatarimage} />
           </div>
@@ -71,33 +204,32 @@ function Navbar() {
           </div>
           {/* dropdown circle */}
           <div>
-          <div className="relative text-nowrap" ref={dropdownRef}>
-      <button onClick={toggleDropdown}>
-        <IoIosArrowDropdownCircle className="text-[11px] md:text-[17px] lg:text-[21px] 2xl:text-[26px]" />
-      </button>
-      {circleDropdown && (
-        <div className="absolute right-3 bg-white border border-gray-300 shadow-md rounded mt-1">
-          <ul>
-            <li
-              className="px-4 py-2 hover:bg-gray-100 cursor-pointer "
-              onClick={() => setCircleDropdown(false)}
-            >
-              Logout
-            </li>
-            <li
-              className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-              onClick={() => {
-                setCircleDropdown(false);
-                navigate("/accountsettings");
-              }}
-            >
-              Account Settings
-            </li>
-          </ul>
-        </div>
-      )}
-    </div>
-            
+            <div className="relative text-nowrap" ref={dropdownRef}>
+              <button onClick={toggleDropdown}>
+                <IoIosArrowDropdownCircle className="text-[11px] md:text-[17px] lg:text-[21px] 2xl:text-[26px]" />
+              </button>
+              {circleDropdown && (
+                <div className="absolute right-3 bg-white border border-gray-300 shadow-md rounded mt-1">
+                  <ul>
+                    <li
+                      className="px-4 py-2 hover:bg-gray-100 cursor-pointer "
+                      onClick={() => setCircleDropdown(false)}
+                    >
+                      Logout
+                    </li>
+                    <li
+                      className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                      onClick={() => {
+                        setCircleDropdown(false);
+                        navigate("/accountsettings");
+                      }}
+                    >
+                      Account Settings
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
